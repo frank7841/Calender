2c227ffa2dcb7818a31def3eccbab5213329ef6b
Frank <franklinewax@gmail.com>
2020-05-09 13:37:41 +0300

    use focus() function to highlight input boxex with errors

--- Day-Calculator/js/script.js
+++ Day-Calculator/js/script.js
@@ -1,11 +1,23 @@
-function validate(){  
+function validate(dayDate){  
 var dayDate=document.myform.dayDate.value;  
 var monthDate=document.myform.monthDate.value;
 var yearDate=document.myform.yearDate.value;
-if (isNaN(dayDate,monthDate)){  
-alert("You must eneter a number");
-return false;  
+if (isNaN(dayDate,monthDate,yearDate)){  
+    alert("You must eneter a valid date");
+    document.myform.dayDate.focus();
+    return false;  
 }
+if (isNaN(monthDate)){
+    alert("You must enter a valid month");
+    document.myform.monthDate.focus();
+    return false;
+}
+if (isNaN(yearDate)) {
+    alert("Enter a valid year");
+    document.myform.yearDate.focus();
+    return false;
+}
+
 else
 return true
 }
